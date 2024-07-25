3a3d582f87c98aec6ee48b36aa25574d89d00196
Denis Carriere <carriere.denis@gmail.com>
2024-07-25 14:05:29 +0200

    add sinks

--- blocks/evm/src/sinks.rs
+++ blocks/evm/src/sinks.rs
@@ -3,13 +3,13 @@ use substreams::pb::substreams::Clock;
 use substreams_database_change::pb::database::DatabaseChanges;
 use substreams_ethereum::pb::eth::v2::Block;
 
-// use crate::blocks::insert_blocks;
+use crate::blocks::insert_blocks;
 use crate::logs::insert_logs;
 
 #[substreams::handlers::map]
 pub fn ch_out(clock: Clock, block: Block) -> Result<DatabaseChanges, Error> {
     let mut tables: DatabaseChanges = DatabaseChanges::default();
-    // insert_blocks(&mut tables, &clock, &block);
-    // insert_logs(&mut tables, &clock, &block);
+    insert_blocks(&mut tables, &clock, &block);
+    insert_logs(&mut tables, &clock, &block);
     Ok(tables)
 }
