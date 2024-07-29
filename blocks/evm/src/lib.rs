300f62cf7673c9252c11c06db5c90d8666be4fb0
Denis Carriere <carriere.denis@gmail.com>
2024-07-29 14:47:29 +0200

    update date

--- blocks/evm/src/lib.rs
+++ blocks/evm/src/lib.rs
@@ -2,3 +2,5 @@ mod balance_changes;
 mod blocks;
 mod logs;
 mod sinks;
+// mod traces;
+// mod transactions;
--- common/src/sinks.rs
+++ common/src/sinks.rs
@@ -11,8 +11,7 @@ pub fn insert_timestamp(row: &mut TableChange, clock: &Clock, is_block: bool) {
     let block_hash = format!("0x{}", clock.id);
     let prefix = if is_block { "" } else { "block_" };
 
-    row
-        .change(format!("{}date", prefix).as_str(), ("", block_date.as_str()))
+    row.change(format!("{}date", prefix).as_str(), ("", block_date.as_str()))
         .change(format!("{}time", prefix).as_str(), ("", block_time.as_str()))
         .change(format!("{}number", prefix).as_str(), ("", block_number.as_str()))
         .change(format!("{}hash", prefix).as_str(), ("", block_hash.as_str()));
