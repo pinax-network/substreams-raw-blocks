ec8d05f6a06b943417f12da7b27c6a5e30feaac0
Denis Carriere <carriere.denis@gmail.com>
2024-08-02 19:11:55 -0400

    fix traces

--- blocks/evm/src/traces.rs
+++ blocks/evm/src/traces.rs
@@ -33,64 +33,63 @@ pub fn insert_trace(tables: &mut DatabaseChanges, clock: &Clock, call: &CallView
     let tx_success = is_transaction_success(transaction.status);
 
     // traces
-    for trace in transaction.calls.iter() {
-        let address = bytes_to_hex(trace.address.clone()); // additional `trace_address`?
-        let begin_ordinal = trace.begin_ordinal;
-        let call_type = call_types_to_string(trace.call_type);
-        let call_type_code = trace.call_type;
-        let caller = bytes_to_hex(trace.caller.clone());
-        let depth = trace.depth;
-        let end_ordinal = trace.end_ordinal;
-        let executed_code = trace.executed_code;
-        let failure_reason = &trace.failure_reason;
-        let gas_consumed = trace.gas_consumed;
-        let gas_limit = trace.gas_limit;
-        let index = trace.index; // or `subtraces`?
-        let input = bytes_to_hex(trace.input.clone());
-        let parent_index = trace.parent_index;
-        let return_data = bytes_to_hex(trace.return_data.clone());
-        let state_reverted = trace.state_reverted;
-        let status_failed = trace.status_failed;
-        let status_reverted = trace.status_reverted;
-        let suicide = trace.suicide; // or `selfdestruct`?
-        let value = optional_bigint_to_string(trace.value.clone()); // UInt256
+    let trace = call.call;
+    let address = bytes_to_hex(trace.address.clone()); // additional `trace_address`?
+    let begin_ordinal = trace.begin_ordinal;
+    let call_type = call_types_to_string(trace.call_type);
+    let call_type_code = trace.call_type;
+    let caller = bytes_to_hex(trace.caller.clone());
+    let depth = trace.depth;
+    let end_ordinal = trace.end_ordinal;
+    let executed_code = trace.executed_code;
+    let failure_reason = &trace.failure_reason;
+    let gas_consumed = trace.gas_consumed;
+    let gas_limit = trace.gas_limit;
+    let index = trace.index; // or `subtraces`?
+    let input = bytes_to_hex(trace.input.clone());
+    let parent_index = trace.parent_index;
+    let return_data = bytes_to_hex(trace.return_data.clone());
+    let state_reverted = trace.state_reverted;
+    let status_failed = trace.status_failed;
+    let status_reverted = trace.status_reverted;
+    let suicide = trace.suicide; // or `selfdestruct`?
+    let value = optional_bigint_to_string(trace.value.clone()); // UInt256
 
-        // TODO: trace.code_changes
-        // TODO: trace.storage_changes
+    // TODO: trace.code_changes
+    // TODO: trace.storage_changes
 
-        let keys = traces_keys(&clock, &tx_hash, &tx_index, &index);
-        let row = tables
-            .push_change_composite("traces", keys, 0, table_change::Operation::Create)
-            // transaction
-            .change("tx_index", ("", tx_index.to_string().as_str()))
-            .change("tx_hash", ("", tx_hash.as_str()))
-            .change("from", ("", from.as_str()))
-            .change("to", ("", to.as_str()))
-            .change("tx_status", ("", tx_status.as_str()))
-            .change("tx_status_code", ("", tx_status_code.to_string().as_str()))
-            .change("tx_success", ("", tx_success.to_string().as_str()))
-            // trace
-            .change("address", ("", address.as_str()))
-            .change("begin_ordinal", ("", begin_ordinal.to_string().as_str()))
-            .change("call_type", ("", call_type.as_str()))
-            .change("call_type_code", ("", call_type_code.to_string().as_str()))
-            .change("caller", ("", caller.as_str()))
-            .change("depth", ("", depth.to_string().as_str()))
-            .change("end_ordinal", ("", end_ordinal.to_string().as_str()))
-            .change("executed_code", ("", executed_code.to_string().as_str()))
-            .change("failure_reason", ("", failure_reason.as_str()))
-            .change("gas_consumed", ("", gas_consumed.to_string().as_str()))
-            .change("gas_limit", ("", gas_limit.to_string().as_str()))
-            .change("index", ("", index.to_string().as_str()))
-            .change("input", ("", input.as_str()))
-            .change("parent_index", ("", parent_index.to_string().as_str()))
-            .change("return_data", ("", return_data.as_str()))
-            .change("state_reverted", ("", state_reverted.to_string().as_str()))
-            .change("status_failed", ("", status_failed.to_string().as_str()))
-            .change("status_reverted", ("", status_reverted.to_string().as_str()))
-            .change("suicide", ("", suicide.to_string().as_str()))
-            .change("value", ("", value.as_str()));
+    let keys = traces_keys(&clock, &tx_hash, &tx_index, &index);
+    let row = tables
+        .push_change_composite("traces", keys, 0, table_change::Operation::Create)
+        // transaction
+        .change("tx_index", ("", tx_index.to_string().as_str()))
+        .change("tx_hash", ("", tx_hash.as_str()))
+        .change("from", ("", from.as_str()))
+        .change("to", ("", to.as_str()))
+        .change("tx_status", ("", tx_status.as_str()))
+        .change("tx_status_code", ("", tx_status_code.to_string().as_str()))
+        .change("tx_success", ("", tx_success.to_string().as_str()))
+        // trace
+        .change("address", ("", address.as_str()))
+        .change("begin_ordinal", ("", begin_ordinal.to_string().as_str()))
+        .change("call_type", ("", call_type.as_str()))
+        .change("call_type_code", ("", call_type_code.to_string().as_str()))
+        .change("caller", ("", caller.as_str()))
+        .change("depth", ("", depth.to_string().as_str()))
+        .change("end_ordinal", ("", end_ordinal.to_string().as_str()))
+        .change("executed_code", ("", executed_code.to_string().as_str()))
+        .change("failure_reason", ("", failure_reason.as_str()))
+        .change("gas_consumed", ("", gas_consumed.to_string().as_str()))
+        .change("gas_limit", ("", gas_limit.to_string().as_str()))
+        .change("index", ("", index.to_string().as_str()))
+        .change("input", ("", input.as_str()))
+        .change("parent_index", ("", parent_index.to_string().as_str()))
+        .change("return_data", ("", return_data.as_str()))
+        .change("state_reverted", ("", state_reverted.to_string().as_str()))
+        .change("status_failed", ("", status_failed.to_string().as_str()))
+        .change("status_reverted", ("", status_reverted.to_string().as_str()))
+        .change("suicide", ("", suicide.to_string().as_str()))
+        .change("value", ("", value.as_str()));
 
-        insert_timestamp(row, clock, false);
-    }
+    insert_timestamp(row, clock, false);
 }
