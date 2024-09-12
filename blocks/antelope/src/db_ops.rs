ee879cc7f5d66e15de3c48fd75ffd27f0836eba3
zolting <44510235+zolting@users.noreply.github.com>
2024-09-12 11:54:46 -0400

    Remove db_ops reformatting + check off featureop

--- blocks/antelope/src/db_ops.rs
+++ blocks/antelope/src/db_ops.rs
@@ -1,59 +1,73 @@
 use common::blocks::insert_timestamp;
 use substreams::{pb::substreams::Clock, Hex};
-use substreams_antelope::pb::{DbOp, TransactionTrace};
 use substreams_database_change::pb::database::{table_change, DatabaseChanges};
+use substreams_antelope::pb::Block;
 
-use crate::keys::db_ops_keys;
+use crate::keys::blocks_keys;
 
-use super::transactions::insert_transaction_metadata;
+use super::{size::insert_size, transactions::insert_transaction};
 
-pub fn operation_to_string(operation: i32) -> String {
-    match operation {
-        0 => "Unknown".to_string(),
-        1 => "Insert".to_string(),
-        2 => "Update".to_string(),
-        3 => "Remove".to_string(),
-        _ => "Unknown".to_string(),
-    }
-}
+// https://github.com/pinax-network/firehose-antelope/blob/534ca5bf2aeda67e8ef07a1af8fc8e0fe46473ee/proto/sf/antelope/type/v1/type.proto#L21
+pub fn insert_blocks(tables: &mut DatabaseChanges, clock: &Clock, block: &Block) {
+    // header
+    let header = block.header.clone().unwrap_or_default();
+    let parent_hash = &header.previous; // renamed to follow EVM block schema
+    let producer = &header.producer;
+    let confirmed = &header.confirmed;
+    let schedule_version = &header.schedule_version;
 
-// https://github.com/streamingfast/firehose-ethereum/blob/1bcb32a8eb3e43347972b6b5c9b1fcc4a08c751e/proto/sf/ethereum/type/v2/type.proto#L647
-pub fn insert_db_op(tables: &mut DatabaseChanges, clock: &Clock, db_op: &DbOp, transaction: &TransactionTrace, index: u32) {
-    let operation = operation_to_string(db_op.operation);
-    let operation_code = db_op.operation;
-    let action_index = db_op.action_index;
-    let code = &db_op.code;
-    let scope = &db_op.scope;
-    let table_name = &db_op.table_name;
-    let primary_key = &db_op.primary_key;
-    let old_payer = &db_op.old_payer;
-    let new_payer = &db_op.new_payer;
-    let old_data = Hex::encode(&db_op.old_data.to_vec());
-    let new_data = Hex::encode(&db_op.new_data.to_vec());
-    let old_data_json = &db_op.old_data_json;
-    let new_data_json = &db_op.new_data_json;
+    // block
+    let version = block.version;
+    let producer_signature = &block.producer_signature;
+    let dpos_proposed_irreversible_blocknum = block.dpos_proposed_irreversible_blocknum;
+    let dpos_irreversible_blocknum = block.dpos_irreversible_blocknum;
 
-    // transaction
-    let tx_hash = &transaction.id;
+    // blockroot_merkle
+    let blockroot_merkle = block.blockroot_merkle.clone().unwrap_or_default();
 
-    let keys = db_ops_keys(clock, &tx_hash, &index);
+    // TO-DO
+    // Array(String) type is not supported by `substreams-sink-sql`
+    // https://github.com/pinax-network/substreams-sink-sql/issues/18
+    // let blockroot_merkle_active_nodes = blockroot_merkle.active_nodes.iter().map(|row| bytes_to_hex(row).to_string()).collect::<Vec<String>>();
+    let blockroot_merkle_node_count = blockroot_merkle.node_count;
+
+    // block roots
+    let transaction_mroot = Hex::encode(&header.transaction_mroot.to_vec());
+    let action_mroot = Hex::encode(&header.action_mroot.to_vec());
+
+    // TO-DO
+    // to be used during Legacy to Savanna transition where action_mroot needs to be converted from Legacy merkle to Savanna merkle
+    // let action_mroot_savanna = block.action_mroot_savanna;
+
+    // blocks
+    let keys = blocks_keys(&clock);
     let row = tables
-        .push_change_composite("db_ops", keys, 0, table_change::Operation::Create)
-        .change("index", ("", index.to_string().as_str()))
-        .change("operation", ("", operation.to_string().as_str()))
-        .change("operation_code", ("", operation_code.to_string().as_str()))
-        .change("action_index", ("", action_index.to_string().as_str()))
-        .change("code", ("", code.to_string().as_str()))
-        .change("scope", ("", scope.to_string().as_str()))
-        .change("table_name", ("", table_name.to_string().as_str()))
-        .change("primary_key", ("", primary_key.to_string().as_str()))
-        .change("old_payer", ("", old_payer.to_string().as_str()))
-        .change("new_payer", ("", new_payer.to_string().as_str()))
-        .change("old_data", ("", old_data.to_string().as_str()))
-        .change("new_data", ("", new_data.to_string().as_str()))
-        .change("old_data_json", ("", old_data_json.to_string().as_str()))
-        .change("new_data_json", ("", new_data_json.to_string().as_str()));
+        .push_change_composite("blocks", keys, 0, table_change::Operation::Create)
+        // header
+        .change("parent_hash", ("", parent_hash.as_str()))
+        .change("producer", ("", producer.to_string().as_str()))
+        .change("confirmed", ("", confirmed.to_string().as_str()))
+        .change("schedule_version", ("", schedule_version.to_string().as_str()))
 
-    insert_timestamp(row, clock, false, false);
-    insert_transaction_metadata(row, transaction);
+        // block
+        .change("version", ("", version.to_string().as_str()))
+        .change("producer_signature", ("", producer_signature.to_string().as_str()))
+        .change("dpos_proposed_irreversible_blocknum", ("", dpos_proposed_irreversible_blocknum.to_string().as_str()))
+        .change("dpos_irreversible_blocknum", ("", dpos_irreversible_blocknum.to_string().as_str()))
+
+        // block roots
+        .change("transaction_mroot", ("", transaction_mroot.to_string().as_str()))
+        .change("action_mroot", ("", action_mroot.to_string().as_str()))
+        // .change("blockroot_merkle_active_nodes", ("", format!("['{}']", blockroot_merkle_active_nodes.join("','") ).as_str()))
+        .change("blockroot_merkle_node_count", ("", blockroot_merkle_node_count.to_string().as_str()))
+        ;
+
+    // transaction status counts
+    insert_size(row, block);
+    insert_timestamp(row, clock, true, false);
+
+    // TABLE::transactions
+    for transaction in block.transaction_traces() {
+        insert_transaction(tables, clock, &transaction, &header);
+    }
 }
