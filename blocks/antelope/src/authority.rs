dd4eaab9806adc2299fc71fcf27e28f7af4e8602
zolting <44510235+zolting@users.noreply.github.com>
2024-11-18 14:22:46 -0500

    Add Accounts/Keys/Waits

--- blocks/antelope/src/authority.rs
+++ blocks/antelope/src/authority.rs
@@ -1,8 +1,92 @@
-use crate::{keys::authority_keys, transactions::insert_transaction_metadata};
-use common::blocks::insert_timestamp;
+use crate::{
+    keys::authority_keys,
+    pb::antelope::{Account as RawAccount, Key as RawKey, Wait as RawWait},
+    transactions::{insert_transaction_metadata, is_transaction_success},
+};
+use common::{blocks::insert_timestamp, structs::BlockTimestamp};
 use substreams::pb::substreams::Clock;
-use substreams_antelope::pb::{Authority, TransactionTrace};
+use substreams_antelope::{
+    pb::{Authority, TransactionTrace},
+    Block,
+};
 use substreams_database_change::pb::database::{table_change, DatabaseChanges};
+
+pub struct AuthorityVectors {
+    pub accounts: Vec<RawAccount>,
+    pub keys: Vec<RawKey>,
+    pub waits: Vec<RawWait>,
+}
+
+pub fn collect_authority_vectors(block: &Block, timestamp: &BlockTimestamp) -> AuthorityVectors {
+    let mut accounts: Vec<RawAccount> = vec![];
+    let mut keys: Vec<RawKey> = vec![];
+    let mut waits: Vec<RawWait> = vec![];
+
+    for transaction in block.transaction_traces() {
+        let tx_hash = &transaction.id;
+        let tx_success = is_transaction_success(transaction.receipt.clone().unwrap_or_default().status);
+
+        for perm_op in transaction.perm_ops.iter() {
+            if let Some(new_perm) = &perm_op.new_perm {
+                let authority = new_perm.authority.as_ref().unwrap();
+                let action_index = perm_op.action_index;
+
+                // Process authority
+                for (index, account) in authority.accounts.iter().enumerate() {
+                    if let Some(permission) = &account.permission {
+                        accounts.push(RawAccount {
+                            block_time: Some(timestamp.time.clone()),
+                            block_number: timestamp.number,
+                            block_hash: timestamp.hash.clone(),
+                            block_date: timestamp.date.clone(),
+                            tx_hash: tx_hash.clone(),
+                            tx_success,
+                            index: index as u32,
+                            action_index: perm_op.action_index as u32,
+                            actor: permission.actor.clone(),
+                            permission: permission.permission.clone(),
+                            weight: account.weight,
+                        });
+                    }
+                }
+
+                // Process keys
+                for (index, key) in authority.keys.iter().enumerate() {
+                    keys.push(RawKey {
+                        block_time: Some(timestamp.time.clone()),
+                        block_number: timestamp.number,
+                        block_hash: timestamp.hash.clone(),
+                        block_date: timestamp.date.clone(),
+                        tx_hash: tx_hash.clone(),
+                        tx_success,
+                        index: index as u32,
+                        action_index: action_index as u32,
+                        public_key: key.public_key.clone(),
+                        weight: key.weight,
+                    });
+                }
+
+                // Process waits
+                for (index, wait) in authority.waits.iter().enumerate() {
+                    waits.push(RawWait {
+                        block_time: Some(timestamp.time.clone()),
+                        block_number: timestamp.number,
+                        block_hash: timestamp.hash.clone(),
+                        block_date: timestamp.date.clone(),
+                        tx_hash: tx_hash.clone(),
+                        tx_success,
+                        index: index as u32,
+                        action_index: action_index as u32,
+                        wait_sec: wait.wait_sec,
+                        weight: wait.weight,
+                    });
+                }
+            }
+        }
+    }
+
+    AuthorityVectors { accounts, keys, waits }
+}
 
 pub fn insert_authority(tables: &mut DatabaseChanges, clock: &Clock, transaction: &TransactionTrace, action_index: u32, authority: &Authority) {
     let mut index = 0;
--- blocks/antelope/src/sinks.rs
+++ blocks/antelope/src/sinks.rs
@@ -4,13 +4,14 @@ use substreams::pb::substreams::Clock;
 use substreams_antelope::pb::Block;
 
 use crate::{
-    actions::collect_actions, blocks::collect_block, db_ops::collect_db_ops, feature_ops::collect_feature_ops, pb::antelope::Events, perm_ops::collect_perm_ops, table_ops::collect_table_ops,
-    transactions::collect_transactions,
+    actions::collect_actions, authority::collect_authority_vectors, blocks::collect_block, db_ops::collect_db_ops, feature_ops::collect_feature_ops, pb::antelope::Events, perm_ops::collect_perm_ops,
+    table_ops::collect_table_ops, transactions::collect_transactions,
 };
 
 #[substreams::handlers::map]
 pub fn map_events(clock: Clock, block: Block) -> Result<Events, Error> {
     let timestamp = build_timestamp(&clock);
+    let authority_vectors = collect_authority_vectors(&block, &timestamp);
 
     Ok(Events {
         blocks: vec![collect_block(&block, &timestamp)],
@@ -20,9 +21,9 @@ pub fn map_events(clock: Clock, block: Block) -> Result<Events, Error> {
         feature_ops: collect_feature_ops(&block, &timestamp),
         perm_ops: collect_perm_ops(&block, &timestamp),
         table_ops: collect_table_ops(&block, &timestamp),
-        accounts: vec![],
-        keys: vec![],
-        waits: vec![],
+        accounts: authority_vectors.accounts,
+        keys: authority_vectors.keys,
+        waits: authority_vectors.waits,
         ram_ops: vec![],
         authorizations: vec![],
         auth_sequences: vec![],
