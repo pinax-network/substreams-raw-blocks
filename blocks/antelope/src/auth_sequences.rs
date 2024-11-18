6f7cebe16a1fa0a3aebd4fd6db650cc0a7ca59a8
zolting <choinieredillan@gmail.com>
2024-11-18 17:21:32 -0500

    Add AuthSequence

--- blocks/antelope/src/auth_sequences.rs
+++ blocks/antelope/src/auth_sequences.rs
@@ -1,11 +1,15 @@
 use common::blocks::insert_timestamp;
+use common::structs::BlockTimestamp;
 use substreams::pb::substreams::Clock;
 use substreams_antelope::pb::AuthSequence;
 use substreams_antelope::pb::{ActionTrace, TransactionTrace};
+use substreams_antelope::Block;
 use substreams_database_change::pb::database::{table_change, DatabaseChanges};
 
 use crate::keys::auth_sequence_keys;
+use crate::pb::antelope::AuthSequence as RawAuthSequence;
 use crate::transactions::insert_transaction_metadata;
+use crate::transactions::is_transaction_success;
 
 pub fn insert_auth_sequence(tables: &mut DatabaseChanges, clock: &Clock, action: &ActionTrace, transaction: &TransactionTrace, auth_sequence: &AuthSequence, index: &u32) {
     // transaction
@@ -27,4 +31,36 @@ pub fn insert_auth_sequence(tables: &mut DatabaseChanges, clock: &Clock, action:
 
     insert_transaction_metadata(row, transaction);
     insert_timestamp(row, clock, false, false);
+}
+
+pub fn collect_auth_sequences(block: &Block, timestamp: &BlockTimestamp) -> Vec<RawAuthSequence> {
+    let mut auth_sequences: Vec<RawAuthSequence> = vec![];
+
+    for transaction in block.transaction_traces() {
+        let tx_hash = &transaction.id;
+        let tx_success = is_transaction_success(transaction.receipt.clone().unwrap_or_default().status);
+
+        for action_trace in transaction.action_traces.iter() {
+            let receipt = action_trace.receipt.as_ref().expect("Action trace receipt is required");
+
+            let action_index = action_trace.execution_index;
+
+            for (index, auth) in receipt.auth_sequence.iter().enumerate() {
+                auth_sequences.push(RawAuthSequence {
+                    block_time: Some(timestamp.time.clone()),
+                    block_number: timestamp.number,
+                    block_hash: timestamp.hash.clone(),
+                    block_date: timestamp.date.clone(),
+                    tx_hash: tx_hash.clone(),
+                    tx_success,
+                    action_index,
+                    index: index as u32,
+                    account_name: auth.account_name.clone(),
+                    sequence: auth.sequence,
+                });
+            }
+        }
+    }
+
+    auth_sequences
 }
--- blocks/antelope/src/sinks.rs
+++ blocks/antelope/src/sinks.rs
@@ -4,8 +4,8 @@ use substreams::pb::substreams::Clock;
 use substreams_antelope::pb::Block;
 
 use crate::{
-    actions::collect_actions, authority::collect_authority_vectors, authorizations::collect_authorizations, blocks::collect_block, db_ops::collect_db_ops, feature_ops::collect_feature_ops,
-    pb::antelope::Events, perm_ops::collect_perm_ops, ram_ops::collect_ram_ops, table_ops::collect_table_ops, transactions::collect_transactions,
+    actions::collect_actions, auth_sequences::collect_auth_sequences, authority::collect_authority_vectors, authorizations::collect_authorizations, blocks::collect_block, db_ops::collect_db_ops,
+    feature_ops::collect_feature_ops, pb::antelope::Events, perm_ops::collect_perm_ops, ram_ops::collect_ram_ops, table_ops::collect_table_ops, transactions::collect_transactions,
 };
 
 #[substreams::handlers::map]
@@ -26,7 +26,7 @@ pub fn map_events(clock: Clock, block: Block) -> Result<Events, Error> {
         waits: authority_vectors.waits,
         ram_ops: collect_ram_ops(&block, &timestamp),
         authorizations: collect_authorizations(&block, &timestamp),
-        auth_sequences: vec![],
+        auth_sequences: collect_auth_sequences(&block, &timestamp),
         account_ram_deltas: vec![],
         creation_trees: vec![],
     })
