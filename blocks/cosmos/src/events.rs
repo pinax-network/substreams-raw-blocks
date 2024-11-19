8dcf1a8fa86a10e344579261fa1c94157745d4e8
Dillan Choinière <choinieredillan@gmail.com>
2024-11-19 11:45:20 -0500

    Fix TransactionEvent

--- blocks/cosmos/src/events.rs
+++ blocks/cosmos/src/events.rs
@@ -4,23 +4,29 @@ use substreams_cosmos::Block;
 
 use crate::{
     pb::cosmos::{BlockEvent as RawBlockEvent, TransactionEvent as RawTransactionEvent},
-    utils::build_attributes_array_string,
+    utils::{build_attributes_array_string, compute_tx_hash},
 };
 
 pub fn collect_tx_events(block: &Block, timestamp: &BlockTimestamp) -> Vec<RawTransactionEvent> {
     let mut vec: Vec<RawTransactionEvent> = vec![];
 
-    for (index, event) in block.events.iter().enumerate() {
-        vec.push(RawTransactionEvent {
-            block_time: Some(timestamp.time),
-            block_number: timestamp.number,
-            block_date: timestamp.date.clone(),
-            block_hash: Hex::encode(&block.hash),
-            index: index as u32,
-            tx_hash: Hex::encode(&block.hash),
-            r#type: event.r#type.clone(),
-            attributes: build_attributes_array_string(&event.attributes),
-        });
+
+    for (tx_index, tx) in block.tx_results.iter().enumerate() {
+        for (event_index, event) in tx.events.iter().enumerate() {
+            let tx_as_bytes = block.txs.get(tx_index).unwrap();
+            let tx_hash = compute_tx_hash(tx_as_bytes);
+
+            vec.push(RawTransactionEvent {
+                block_time: Some(timestamp.time),
+                block_number: timestamp.number,
+                block_date: timestamp.date.clone(),
+                block_hash: Hex::encode(&block.hash),
+                index: event_index as u32,
+                tx_hash: Hex::encode(&tx_hash),
+                r#type: event.r#type.clone(),
+                attributes: build_attributes_array_string(&event.attributes),
+            });
+        }
     }
     vec
 }
