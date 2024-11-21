e2666df65e2f5a58a5c63d4b9483659666cc4e9e
Denis Carriere <carriere.denis@gmail.com>
2024-11-21 15:43:31 +0100

    comment out bitcoin

--- blocks/bitcoin/src/events.rs
+++ blocks/bitcoin/src/events.rs
@@ -4,6 +4,6 @@ use substreams_bitcoin::pb::btc::v1::Block;
 use crate::pb::bitcoin::Events;
 
 #[substreams::handlers::map]
-pub fn map_events(block: Block, clock: Clock) -> Result<Events, Error> {
+pub fn map_events(_block: Block, _clock: Clock) -> Result<Events, Error> {
     Ok(Events::default())
 }
